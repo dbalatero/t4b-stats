cc1153f0fa0e75c8a4294e27afb37d197d84f7ef
David Balatero <dbalatero@gmail.com>
2016-04-08 11:32:21 -0700

    Re-raise errors, dump out info

--- app/services/update_assignments.rb
+++ app/services/update_assignments.rb
@@ -7,9 +7,14 @@ class UpdateAssignments
     UpdateColorCells.run
 
     FetchSpreadsheetRows.run.each do |row|
-      assignment = Assignment.find_or_initialize_by(filename: row[:filename])
-      assignment.attributes = row
-      assignment.save!
+      begin
+        assignment = Assignment.find_or_initialize_by(filename: row[:filename])
+        assignment.attributes = row
+        assignment.save!
+      rescue
+        puts "Got attributes for bad record: #{attributes.inspect}"
+        raise
+      end
     end
 
     true
